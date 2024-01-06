1e49b1f0819ac2c8b68020fc598971165b351600
jlkim909 <jlkim909@gmail.com>
2024-01-06 22:50:58 +0900

    fix : PhotoMemo 주석 삭제

--- app/src/main/java/com/hifi/redeal/memo/PhotoDetailFragment.kt
+++ app/src/main/java/com/hifi/redeal/memo/PhotoDetailFragment.kt
@@ -1,7 +1,6 @@
 package com.hifi.redeal.memo
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -22,14 +21,14 @@ class PhotoDetailFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         return ComposeView(
             requireContext()
         ).apply {
             val mainActivity = activity as MainActivity
             val imgSrcArr = arguments?.getStringArrayList("srcArr")!!
             val imgOrder = arguments?.getInt("order")!!
-            Log.d("testaaa", "${imgSrcArr}")
+
             setViewCompositionStrategy(
                 ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
             )
@@ -46,116 +45,3 @@ class PhotoDetailFragment : Fragment() {
         }
     }
 }
-//        fragmentPhotoDetailBinding = FragmentPhotoDetailBinding.inflate(inflater)
-//        mainActivity = activity as MainActivity
-//        imgOrder = arguments?.getInt("order")!!
-//        imgSrcArr = arguments?.getStringArrayList("srcArr")!!
-//
-//        photoMemoRepository.getPhotoMemoImgUrl(imgSrcArr[imgOrder]) { url ->
-//            Glide.with(fragmentPhotoDetailBinding.detailImageView)
-//                .load(url)
-//                .into(fragmentPhotoDetailBinding.detailImageView)
-//        }
-//        gestureDetector = GestureDetector(requireContext(), SwipeGestureListener {
-//            if (it == SwipeGestureListener.Direction.LEFT) {
-//                if (imgOrder + 1 >= imgSrcArr.size) {
-//                    return@SwipeGestureListener
-//                }
-//                imgOrder++
-//            } else if (it == SwipeGestureListener.Direction.RIGHT) {
-//                if (imgOrder - 1 < 0) {
-//                    return@SwipeGestureListener
-//                }
-//                imgOrder--
-//            }
-//            photoMemoRepository.getPhotoMemoImgUrl(imgSrcArr[imgOrder]) { url ->
-//                Glide.with(fragmentPhotoDetailBinding.detailImageView)
-//                    .load(url)
-//                    .into(fragmentPhotoDetailBinding.detailImageView)
-//            }
-//            if (previousView != null) {
-//                previousView.background = null
-//            }
-//            previousView = fragmentPhotoDetailBinding.photoDetailRecyclerView[imgOrder]
-//            fragmentPhotoDetailBinding.photoDetailRecyclerView[imgOrder].setBackgroundResource(R.drawable.row_photo_detail_border)
-//        })
-//
-//        fragmentPhotoDetailBinding.run {
-//            photoDetailToolbar.run {
-//                setNavigationOnClickListener {
-//                    mainActivity.removeFragment(MainActivity.PHOTO_DETAIL_FRAGMENT)
-//                }
-//            }
-//            detailImageView.run {
-//                setImageResource(R.drawable.empty_photo)
-//                setOnTouchListener { _, event ->
-//                    gestureDetector.onTouchEvent(event)
-//                    true
-//                }
-//            }
-//            photoDetailRecyclerView.run {
-//                adapter = PhotoDetailRecyclerAdapter()
-//                layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-//            }
-//        }
-//        return fragmentPhotoDetailBinding.root
-//    }
-//
-//    inner class PhotoDetailRecyclerAdapter :
-//        RecyclerView.Adapter<PhotoDetailRecyclerAdapter.PhotoDetailViewHolder>() {
-//        inner class PhotoDetailViewHolder(rowPhotoDetailBinding: RowDetailPhotoBinding) :
-//            RecyclerView.ViewHolder(rowPhotoDetailBinding.root) {
-//            val rowImageView = rowPhotoDetailBinding.rowImageView
-//
-//            init {
-//                rowImageView.setOnClickListener {
-//                    imgOrder = adapterPosition
-//                    photoMemoRepository.getPhotoMemoImgUrl(imgSrcArr[adapterPosition]) { url ->
-//                        Glide.with(fragmentPhotoDetailBinding.detailImageView)
-//                            .load(url)
-//                            .into(fragmentPhotoDetailBinding.detailImageView)
-//                    }
-//                    if (previousView != null) {
-//                        previousView.background = null
-//                    }
-//                    previousView = rowImageView
-//                    rowImageView.setBackgroundResource(R.drawable.row_photo_detail_border)
-//                }
-//            }
-//
-//            fun bindItem(imgSrc: String) {
-//                if (imgOrder == adapterPosition) {
-//                    previousView = rowImageView
-//                    rowImageView.setBackgroundResource(R.drawable.row_photo_detail_border)
-//                }
-//                rowImageView.setImageResource(R.drawable.empty_photo)
-//                photoMemoRepository.getPhotoMemoImgUrl(imgSrc) { url ->
-//                    Glide.with(rowImageView)
-//                        .load(url)
-//                        .into(rowImageView)
-//                }
-//            }
-//        }
-//
-//        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoDetailViewHolder {
-//            val rowPhotoDetailBinding = RowDetailPhotoBinding.inflate(layoutInflater)
-//            val photoDetailViewHolder = PhotoDetailViewHolder(rowPhotoDetailBinding)
-//
-//            rowPhotoDetailBinding.root.layoutParams = ViewGroup.LayoutParams(
-//                ViewGroup.LayoutParams.WRAP_CONTENT,
-//                ViewGroup.LayoutParams.WRAP_CONTENT
-//            )
-//
-//            return photoDetailViewHolder
-//        }
-//
-//        override fun getItemCount(): Int {
-//            return imgSrcArr.size
-//        }
-//
-//        override fun onBindViewHolder(holder: PhotoDetailViewHolder, position: Int) {
-//            val item = imgSrcArr[position]
-//            holder.bindItem(item)
-//        }
-//    }
-//}
