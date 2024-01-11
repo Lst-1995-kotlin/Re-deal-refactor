48431b0c626e183359a6d29f2ff55cf7d6fed35c
jlkim909 <jlkim909@gmail.com>
2024-01-11 23:51:45 +0900

    fix : PlayState class 생성

--- app/src/main/java/com/hifi/redeal/memo/model/MemoData.kt
+++ app/src/main/java/com/hifi/redeal/memo/model/MemoData.kt
@@ -4,3 +4,9 @@ import android.net.Uri
 import com.google.firebase.Timestamp
 data class UserRecordMemoData(val clientIdx:Long, val context:String, val date:Timestamp, val audioSrc:Uri?, val audioFilename:String)
 data class RecordMemoData(val context:String, val date:Timestamp, val audioFileUri:Uri?, val audioFilename:String)
+
+enum class PlayState {
+    PLAYING,
+    PAUSE,
+    STOP
+}
