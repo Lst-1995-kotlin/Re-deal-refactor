be27f97ea4506a67d225f6f85b414e4d4d5d2012
jlkim909 <jlkim909@gmail.com>
2024-01-11 18:29:20 +0900

    fix : RecordMemoData class 변수 명 변경

--- app/src/main/java/com/hifi/redeal/memo/model/MemoData.kt
+++ app/src/main/java/com/hifi/redeal/memo/model/MemoData.kt
@@ -3,4 +3,4 @@ package com.hifi.redeal.memo.model
 import android.net.Uri
 import com.google.firebase.Timestamp
 data class UserRecordMemoData(val clientIdx:Long, val context:String, val date:Timestamp, val audioSrc:Uri?, val audioFilename:String)
-data class RecordMemoData(val context:String, val date:Timestamp, val audioSrc:Uri?, val audioFilename:String)
+data class RecordMemoData(val context:String, val date:Timestamp, val audioFileUri:Uri?, val audioFilename:String)
