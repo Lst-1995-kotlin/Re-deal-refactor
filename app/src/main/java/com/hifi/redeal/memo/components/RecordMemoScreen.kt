9661829471ebeb9b9d2d9b02c8bea6a37bf9028f
jlkim909 <jlkim909@gmail.com>
2024-01-16 21:59:15 +0900

    fix: ExoPlayer 코드 수정

--- app/src/main/java/com/hifi/redeal/memo/components/RecordMemoScreen.kt
+++ app/src/main/java/com/hifi/redeal/memo/components/RecordMemoScreen.kt
@@ -2,7 +2,6 @@ package com.hifi.redeal.memo.components
 
 import android.os.Bundle
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,7 +10,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.EditNote
@@ -28,7 +26,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -36,6 +33,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -51,10 +49,10 @@ import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import com.hifi.redeal.MainActivity
 import com.hifi.redeal.R
-import com.hifi.redeal.memo.model.PlayState
 import com.hifi.redeal.memo.model.RecordMemoData
 import com.hifi.redeal.memo.utils.intervalBetweenDateText
 import com.hifi.redeal.memo.vm.RecordMemoViewModel
+import java.util.Date
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -116,100 +114,144 @@ private fun RecordMemoToolbar(
 }
 
 @Composable
-private fun MemoBox(
-    modifier: Modifier = Modifier,
-    text: String = "새로운 메모"
+private fun DateText(
+    date: Date,
+    modifier: Modifier = Modifier
 ) {
-    Surface(
-        modifier = modifier,
-        color = Color.White
+    Text(
+        text = intervalBetweenDateText(date),
+        style = MaterialTheme.typography.bodySmall.copy(
+            fontWeight = FontWeight.Bold
+        ),
+        modifier = modifier
     )
-    {
-        Text(
-            text = text,
-            style = MaterialTheme.typography.bodyLarge,
-            modifier = Modifier
-                .border(1.dp, MaterialTheme.colorScheme.primary, RoundedCornerShape(4.dp))
-                .padding(12.dp)
-                .fillMaxWidth()
+}
+
+@Composable
+private fun MemoMultiText(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodyMedium,
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun AudioPlayerToggleButton(
+    isPlaying: Boolean,
+    isFocusing: Boolean,
+    onClickPlayer:() -> Unit,
+    modifier: Modifier = Modifier
+) {
+    OutlinedIconButton(
+        onClick = {
+            onClickPlayer()
+        },
+        colors = IconButtonDefaults.iconButtonColors(
+            containerColor = if (isFocusing && isPlaying) MaterialTheme.colorScheme.primary else Color.White,
+            contentColor = if (isFocusing && isPlaying) Color.White else MaterialTheme.colorScheme.primary
+        ),
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = if (isFocusing && isPlaying) Icons.Default.Pause else Icons.Default.PlayArrow,
+            contentDescription = null
         )
+    }
+}
+
+@Composable
+private fun AudioPlayerFileName(
+    text: String,
+    isFocusing:Boolean,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = text,
+        color = if (isFocusing) MaterialTheme.colorScheme.primary else Color.Black,
+        style = MaterialTheme.typography.bodyLarge.copy(
+            fontWeight = FontWeight.Bold
+        ),
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun AudioPlayerDurationTimeText(
+    text: String,
+    modifier: Modifier = Modifier
+){
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodySmall,
+        modifier = modifier
+    )
+}
+@Composable
+private fun AudioPlayer(
+    audioFilename: String,
+    isPlaying: Boolean,
+    isFocusing:Boolean,
+    onClickPlayer:() -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        AudioPlayerToggleButton(
+            isPlaying = isPlaying,
+            isFocusing = isFocusing,
+            onClickPlayer = onClickPlayer,
+            modifier = Modifier.size(36.dp)
+        )
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp)
+        ) {
+            AudioPlayerFileName(text = audioFilename, isFocusing = isFocusing)
+            AudioPlayerDurationTimeText(text = "")
+            if (isFocusing) {
+                Slider(
+                    value = 0.0f,
+                    onValueChange = {
+                    },
+                    valueRange = 0f..100f, // Define the range of values
+                    steps = 1, // Define the step size
+                    modifier = Modifier
+                )
+            }
+        }
     }
 }
 
 @Composable
 private fun RecordMemoItem(
     item: RecordMemoData,
-    state: PlayState,
-    onClick: () -> Unit,
+    isPlaying:Boolean,
+    isFocusing:Boolean,
+    onClickPlayer:() -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(modifier) {
-        Text(
-            text = intervalBetweenDateText(item.date.toDate()),
-            style = MaterialTheme.typography.bodySmall.copy(
-                fontWeight = FontWeight.ExtraBold
-            ),
+        DateText(
+            date = item.date,
             modifier = Modifier
                 .fillMaxWidth()
         )
-        MemoBox(modifier = Modifier.padding(top = 8.dp), text = item.context)
+        MemoMultiText(modifier = Modifier.padding(top = 8.dp), text = item.context)
 
-        Row(
+        AudioPlayer(
+            audioFilename = item.audioFilename,
+            isPlaying = isPlaying,
+            isFocusing = isFocusing,
+            onClickPlayer = onClickPlayer,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(top = 20.dp)
-        ) {
-            OutlinedIconButton(
-                onClick = {
-                    onClick()
-                },
-                colors = IconButtonDefaults.iconButtonColors(
-                    containerColor = if (state == PlayState.PLAYING) MaterialTheme.colorScheme.primary else Color.White,
-                    contentColor = if (state == PlayState.PLAYING) Color.White else MaterialTheme.colorScheme.primary
-                ),
-                border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
-                modifier = Modifier
-                    .size(36.dp)
-            ) {
-                Icon(
-                    imageVector = if (state == PlayState.PLAYING) Icons.Default.Pause else Icons.Default.PlayArrow,
-                    contentDescription = null,
-                    modifier = Modifier
-                )
-            }
-
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(start = 16.dp)
-            ) {
-                Text(
-                    text = item.audioFilename,
-                    color = if (state != PlayState.STOP) MaterialTheme.colorScheme.primary else Color.Black,
-                    style = MaterialTheme.typography.bodyLarge.copy(
-                        fontWeight = FontWeight.Bold
-                    ),
-                    modifier = Modifier
-                )
-
-                Text(
-                    text = "",
-                    style = MaterialTheme.typography.bodySmall,
-                    modifier = Modifier.padding(top = 8.dp)
-                )
-
-                if (state != PlayState.STOP) {
-                    Slider(
-                        value = 0.0f,
-                        onValueChange = {
-                        },
-                        valueRange = 0f..100f, // Define the range of values
-                        steps = 1, // Define the step size
-                        modifier = Modifier
-                    )
-                }
-            }
-        }
+        )
     }
 }
 
@@ -220,9 +262,8 @@ private fun RecordMemoList(
     modifier: Modifier = Modifier,
 ) {
     val player by remember { mutableStateOf(ExoPlayer.Builder(mainActivity).build()) }
-    var currentPlayingIndex by remember { mutableStateOf(-1) }
-    var playState by remember { mutableStateOf(PlayState.STOP) }
-
+    var currentAudioIndex by remember { mutableIntStateOf(-1) }
+    var isPlaying by remember { mutableStateOf(false) }
 
     DisposableEffect(player) {
         onDispose {
@@ -234,73 +275,27 @@ private fun RecordMemoList(
             itemsIndexed(recordMemoItemList) { idx, item ->
                 RecordMemoItem(
                     item = item,
-                    state = if(currentPlayingIndex == idx) playState else PlayState.STOP,
-                    onClick = {
-                        currentPlayingIndex.takeIf { it != -1 }?.let { player.stop() }
-                        when(playState){
-                            PlayState.STOP -> {
-                                player.setMediaItem(MediaItem.fromUri(item.audioFileUri!!))
-                                player.prepare()
-                                player.play()
-                                playState = PlayState.PLAYING
-                            }
-                            PlayState.PLAYING -> {
-                                playState = if(currentPlayingIndex != idx){
-                                    player.setMediaItem(MediaItem.fromUri(item.audioFileUri!!))
-                                    player.prepare()
-                                    player.play()
-                                    PlayState.PLAYING
-                                }else {
-                                    player.pause()
-                                    PlayState.PAUSE
-                                }
-                            }
-                            PlayState.PAUSE -> {
-                                playState = if(currentPlayingIndex != idx){
-                                    player.setMediaItem(MediaItem.fromUri(item.audioFileUri!!))
-                                    player.prepare()
-                                    player.play()
-                                    PlayState.PLAYING
-                                }else {
-                                    player.prepare()
-                                    player.play()
-                                    PlayState.PLAYING
-                                }
-                            }
-                            else -> {
-
-                            }
+                    isPlaying = isPlaying,
+                    isFocusing = currentAudioIndex == idx,
+                    onClickPlayer = {
+                        if(currentAudioIndex == idx){
+                            isPlaying = !isPlaying
+                            player.playWhenReady = isPlaying
+                        }else{
+                            player.setMediaItem(MediaItem.fromUri(item.audioFileUri!!))
+                            player.prepare()
+                            player.play()
+                            isPlaying = true
                         }
-                        currentPlayingIndex = idx
-                    },
-                    modifier = Modifier.padding(horizontal = 24.dp),
+                        currentAudioIndex = idx
+                    }
                 )
-                Divider(Modifier.padding(vertical = 16.dp))
+                Divider(modifier = Modifier.padding(vertical = 16.dp))
             }
         },
         modifier = modifier
     )
 }
-
-//@Composable
-//private fun RecordMemoListTest(
-//    recordMemoItemList: List<RecordMemoData>,
-//    modifier: Modifier = Modifier,
-//) {
-//    LazyColumn(
-//        content = {
-//            itemsIndexed(recordMemoItemList) { idx, item ->
-//                RecordMemoItemTest(
-//                    item = item,
-//                    isPlaying = idx == 1,
-//                    modifier = Modifier.padding(horizontal = 24.dp),
-//                )
-//                Divider(Modifier.padding(vertical = 16.dp))
-//            }
-//        },
-//        modifier = modifier
-//    )
-//}
 
 @Composable
 fun RecordMemoScreen(
@@ -343,192 +338,8 @@ fun RecordMemoScreen(
                 mainActivity = mainActivity,
                 modifier = Modifier
                     .padding(padding)
-                    .padding(top = 8.dp),
+                    .padding(vertical = 20.dp, horizontal = 24.dp),
             )
         }
     }
 }
-
-//@OptIn(ExperimentalMaterial3Api::class)
-//@Composable
-//private fun RecordMemoToolbarTest(
-//    title: String,
-//    modifier: Modifier = Modifier
-//) {
-//    Box {
-//        TopAppBar(
-//            title = {
-//                Text(
-//                    text = title,
-//                    style = MaterialTheme.typography.bodyLarge.copy(
-//                        fontSize = 20.sp,
-//                        fontWeight = FontWeight.ExtraBold
-//                    )
-//                )
-//            },
-//            navigationIcon = {
-//                IconButton(onClick = {
-//                }) {
-//                    Icon(
-//                        painter = painterResource(R.drawable.arrow_back_ios_24px),
-//                        contentDescription = null,
-//                    )
-//                }
-//            },
-//            actions = {
-//                IconButton(
-//                    onClick = {
-//                        // todo : 편집 버튼 클릭시 삭제, 이름 변경, 즐겨찾기 등록할 수 있도록
-//                    },
-//                    colors = IconButtonDefaults.iconButtonColors(
-//                        contentColor = MaterialTheme.colorScheme.primary
-//                    )
-//                ) {
-//                    Icon(
-//                        imageVector = Icons.Default.EditNote,
-//                        contentDescription = null,
-//                    )
-//                }
-//            },
-//            colors = TopAppBarDefaults.topAppBarColors(
-//                containerColor = Color.White,
-//                navigationIconContentColor = MaterialTheme.colorScheme.primary,
-//                titleContentColor = MaterialTheme.colorScheme.primary
-//            ),
-//            modifier = modifier
-//        )
-//
-//        Divider(
-//            thickness = 2.dp,
-//            color = MaterialTheme.colorScheme.primary,
-//            modifier = Modifier.align(Alignment.BottomStart)
-//        )
-//    }
-//}
-
-@Composable
-private fun RecordMemoItemTest(
-    item: RecordMemoData,
-    isPlaying: Boolean,
-    modifier: Modifier = Modifier
-) {
-    val time = if (isPlaying) "00:00:01 / 00:00:15" else "00:00:15"
-    Column(modifier) {
-        Text(
-            text = intervalBetweenDateText(item.date.toDate()),
-            style = MaterialTheme.typography.bodySmall.copy(
-                fontWeight = FontWeight.ExtraBold
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-        )
-        MemoBox(modifier = Modifier.padding(top = 8.dp), text = item.context)
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 20.dp)
-        ) {
-            OutlinedIconButton(
-                onClick = { /*TODO*/ },
-                colors = IconButtonDefaults.iconButtonColors(
-                    containerColor = if (isPlaying) MaterialTheme.colorScheme.primary else Color.White,
-                    contentColor = if (isPlaying) Color.White else MaterialTheme.colorScheme.primary
-                ),
-                border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
-                modifier = Modifier
-                    .size(36.dp)
-            ) {
-                Icon(
-                    imageVector = if (isPlaying) Icons.Default.Pause else Icons.Default.PlayArrow,
-                    contentDescription = null,
-                    modifier = Modifier
-                )
-            }
-
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(start = 16.dp)
-            ) {
-                Text(
-                    text = item.audioFilename,
-                    color = if (isPlaying) MaterialTheme.colorScheme.primary else Color.Black,
-                    style = MaterialTheme.typography.bodyLarge.copy(
-                        fontWeight = FontWeight.Bold
-                    ),
-                    modifier = Modifier
-                )
-
-                Text(
-                    text = time,
-                    style = MaterialTheme.typography.bodySmall,
-                    modifier = Modifier.padding(top = 8.dp)
-                )
-
-                if (isPlaying) {
-                    Slider(
-                        value = 0.0f,
-                        onValueChange = {
-                        },
-                        valueRange = 0f..100f, // Define the range of values
-                        steps = 1, // Define the step size
-                        modifier = Modifier
-                    )
-                }
-            }
-        }
-    }
-}
-
-//@Preview(name = "RecordMemoScreen")
-//@Composable
-//fun RecordMemoScreenPreView() {
-//    RedealTheme {
-//        Scaffold(
-//            topBar = {
-//                RecordMemoToolbarTest(
-//                    title = stringResource(id = R.string.record_memo_toolbar),
-//                )
-//            },
-//            floatingActionButton = {
-//                FloatingActionButton(
-//                    onClick = {
-//                    },
-//                ) {
-//                    Icon(
-//                        imageVector = Icons.Default.Add,
-//                        contentDescription = null
-//                    )
-//                }
-//            },
-//            floatingActionButtonPosition = FabPosition.End,
-//            containerColor = Color.White
-//        ) { padding ->
-//            testDataList.let {
-//                RecordMemoListTest(
-//                    recordMemoItemList = testDataList,
-//                    modifier = Modifier
-//                        .padding(padding)
-//                        .padding(vertical = 20.dp),
-//                )
-//            }
-//        }
-//    }
-//}
-
-//val testDataList: List<RecordMemoData> = listOf(
-//    RecordMemoData(
-//        "Test Context 1",
-//        Timestamp(Date()),
-//        null,
-//        "test_audio1.mp3"
-//    ),
-//    RecordMemoData(
-//        "Test Context 2",
-//        Timestamp(Date()),
-//        null,
-//        "test_audio2.mp3"
-//    ),
-//    // Add more data as needed...
-//)
