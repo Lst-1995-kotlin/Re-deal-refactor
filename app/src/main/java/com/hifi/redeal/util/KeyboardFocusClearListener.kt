cd1832553dd243aecc798901b582bf4822c24ae3
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-04-01 16:21:47 +0900

    refactor: 매개 변수 명 수정

--- app/src/main/java/com/hifi/redeal/util/KeyboardFocusClearListener.kt
+++ app/src/main/java/com/hifi/redeal/util/KeyboardFocusClearListener.kt
@@ -4,16 +4,16 @@ import android.graphics.Rect
 import android.view.View
 import android.view.ViewTreeObserver
 
-class KeyboardFocusClearListener(private val textView: View) : ViewTreeObserver.OnGlobalLayoutListener {
+class KeyboardFocusClearListener(private val view: View) : ViewTreeObserver.OnGlobalLayoutListener {
 
     override fun onGlobalLayout() {
         val rect = Rect()
-        textView.getWindowVisibleDisplayFrame(rect)
-        val screenHeight = textView.rootView.height
+        view.getWindowVisibleDisplayFrame(rect)
+        val screenHeight = view.rootView.height
         val keypadHeight = screenHeight - rect.bottom
 
         if (keypadHeight < screenHeight * 0.15) {
-            textView.clearFocus()
+            view.clearFocus()
         }
     }
 }
