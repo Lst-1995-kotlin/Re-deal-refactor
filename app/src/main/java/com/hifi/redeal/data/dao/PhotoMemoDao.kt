c1fdccad035a287c6c0cd10fa8fff633119acf12
jlkim909 <jlkim909@gmail.com>
2024-04-04 16:46:11 +0900

    fix : PhotoMemoDao 생성

---
+++ app/src/main/java/com/hifi/redeal/data/dao/PhotoMemoDao.kt
@@ -0,0 +1,28 @@
+package com.hifi.redeal.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.hifi.redeal.memo.model.PhotoMemo
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PhotoMemoDao {
+    @Query("SELECT * FROM photoMemos ORDER BY timestamp ASC")
+    fun getAllPhotoMemo(): Flow<List<PhotoMemo>>
+
+    @Query("SELECT * FROM photoMemos WHERE id = :id")
+    fun getPhotoMemo(id: Int): Flow<PhotoMemo>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertPhotoMemo(photoMemo: PhotoMemo)
+
+    @Update
+    suspend fun updatePhotoMemo(photoMemo: PhotoMemo)
+
+    @Delete
+    suspend fun deletePhotoMemo(photoMemo: PhotoMemo)
+}
