245744cc4fc6b6b3ed3b753b1ad7f00561260aaf
jlkim909 <jlkim909@gmail.com>
2024-04-04 17:05:57 +0900

    fix : PhotoMemoScreen composable 함수 순서 변경

--- app/src/main/java/com/hifi/redeal/memo/components/PhotoMemoScreen.kt
+++ app/src/main/java/com/hifi/redeal/memo/components/PhotoMemoScreen.kt
@@ -41,133 +41,13 @@ import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.hifi.redeal.MainActivity
 import com.hifi.redeal.R
-import com.hifi.redeal.memo.model.PhotoMemoData
+import com.hifi.redeal.memo.model.PhotoMemo
 import com.hifi.redeal.memo.repository.PhotoMemoRepository
 import com.hifi.redeal.memo.ui.MemoTopAppBar
 import com.hifi.redeal.memo.utils.intervalBetweenDateText
 import com.hifi.redeal.memo.vm.PhotoMemoViewModel
 import java.util.Date
 
-@Composable
-private fun DateText(
-    date: Date,
-    modifier: Modifier = Modifier
-) {
-    Text(
-        text = intervalBetweenDateText(date),
-        style = MaterialTheme.typography.bodySmall.copy(
-            fontWeight = FontWeight.Bold
-        ),
-        modifier = modifier
-    )
-}
-
-@Composable
-private fun MemoMultiText(
-    modifier: Modifier = Modifier,
-    text: String
-) {
-    Text(
-        text = text,
-        style = MaterialTheme.typography.bodyMedium,
-        modifier = modifier
-    )
-}
-
-@Composable
-private fun LazyRowImageList(
-    srcArr: List<String>,
-    repository: PhotoMemoRepository,
-    mainActivity: MainActivity,
-    modifier: Modifier = Modifier
-) {
-    LazyRow(
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        contentPadding = PaddingValues(horizontal = 2.dp),
-        content = {
-            itemsIndexed(srcArr) { idx, src ->
-                var imageUrl by remember { mutableStateOf("") }
-                val coroutineScope = rememberCoroutineScope()
-                LaunchedEffect(coroutineScope) {
-                    imageUrl = repository.getPhotoMemoImgUrlToCoroutine(src)
-                }
-                val painter = if (imageUrl == "")
-                    painterResource(id = R.drawable.empty_photo) else
-                    rememberAsyncImagePainter(imageUrl)
-                Image(
-                    painter = painter,
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier
-                        .size(100.dp)
-                        .clip(shape = RoundedCornerShape(8.dp))
-                        .clickable {
-                            val newBundle = Bundle()
-                            newBundle.putInt("order", idx)
-                            newBundle.putStringArrayList(
-                                "srcArr",
-                                srcArr as ArrayList<String>
-                            )
-                            mainActivity.replaceFragment(
-                                MainActivity.PHOTO_DETAIL_FRAGMENT,
-                                true,
-                                newBundle
-                            )
-                        }
-                )
-            }
-        },
-        modifier = modifier
-    )
-}
-
-@Composable
-private fun PhotoMemoItem(
-    item: PhotoMemoData,
-    mainActivity: MainActivity,
-    modifier: Modifier = Modifier,
-    repository: PhotoMemoRepository
-) {
-    Column(modifier) {
-        DateText(
-            date = item.date,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 4.dp)
-        )
-        LazyRowImageList(
-            srcArr = item.srcArr,
-            repository = repository,
-            mainActivity = mainActivity,
-            modifier = Modifier
-                .padding(top = 8.dp)
-                .fillMaxWidth()
-        )
-        MemoMultiText(modifier = Modifier.padding(top = 20.dp, start = 4.dp), text = item.context)
-    }
-}
-
-@Composable
-private fun PhotoMemoList(
-    photoMemoItemList: List<PhotoMemoData>,
-    mainActivity: MainActivity,
-    repository: PhotoMemoRepository,
-    modifier: Modifier = Modifier,
-) {
-    LazyColumn(
-        content = {
-            items(photoMemoItemList) { item ->
-                PhotoMemoItem(
-                    item = item,
-                    mainActivity = mainActivity,
-                    repository = repository
-                )
-                Divider(Modifier.padding(vertical = 16.dp))
-            }
-        },
-        modifier = modifier
-    )
-}
 @Composable
 fun PhotoMemoScreen(
     photoMemoViewModel: PhotoMemoViewModel,
@@ -222,4 +102,125 @@ fun PhotoMemoScreen(
 
         // todo : 리스트 마지막 부분에 맨위로 가는 기능 등 처리 해주기
     }
+}
+
+@Composable
+private fun PhotoMemoList(
+    photoMemoItemList: List<PhotoMemo>,
+    mainActivity: MainActivity,
+    repository: PhotoMemoRepository,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(
+        content = {
+            items(photoMemoItemList) { item ->
+                PhotoMemoItem(
+                    item = item,
+                    mainActivity = mainActivity,
+                    repository = repository
+                )
+                Divider(Modifier.padding(vertical = 16.dp))
+            }
+        },
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun PhotoMemoItem(
+    item: PhotoMemo,
+    mainActivity: MainActivity,
+    modifier: Modifier = Modifier,
+    repository: PhotoMemoRepository
+) {
+    Column(modifier) {
+        DateText(
+            date = item.date,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp)
+        )
+        LazyRowImageList(
+            srcArr = item.imagePaths,
+            repository = repository,
+            mainActivity = mainActivity,
+            modifier = Modifier
+                .padding(top = 8.dp)
+                .fillMaxWidth()
+        )
+        MemoMultiText(modifier = Modifier.padding(top = 20.dp, start = 4.dp), text = item.memo)
+    }
+}
+
+@Composable
+private fun DateText(
+    date: Date,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = intervalBetweenDateText(date),
+        style = MaterialTheme.typography.bodySmall.copy(
+            fontWeight = FontWeight.Bold
+        ),
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun LazyRowImageList(
+    srcArr: List<String>,
+    repository: PhotoMemoRepository,
+    mainActivity: MainActivity,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 2.dp),
+        content = {
+            itemsIndexed(srcArr) { idx, src ->
+                var imageUrl by remember { mutableStateOf("") }
+                val coroutineScope = rememberCoroutineScope()
+                LaunchedEffect(coroutineScope) {
+                    imageUrl = repository.getPhotoMemoImgUrlToCoroutine(src)
+                }
+                val painter = if (imageUrl == "")
+                    painterResource(id = R.drawable.empty_photo) else
+                    rememberAsyncImagePainter(imageUrl)
+                Image(
+                    painter = painter,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(shape = RoundedCornerShape(8.dp))
+                        .clickable {
+                            val newBundle = Bundle()
+                            newBundle.putInt("order", idx)
+                            newBundle.putStringArrayList(
+                                "srcArr",
+                                srcArr as ArrayList<String>
+                            )
+                            mainActivity.replaceFragment(
+                                MainActivity.PHOTO_DETAIL_FRAGMENT,
+                                true,
+                                newBundle
+                            )
+                        }
+                )
+            }
+        },
+        modifier = modifier
+    )
+}
+
+@Composable
+private fun MemoMultiText(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.bodyMedium,
+        modifier = modifier
+    )
 }
