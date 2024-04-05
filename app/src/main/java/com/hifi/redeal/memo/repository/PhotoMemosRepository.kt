6b2f12d1c454971e0456c2bb3a83136ef6103bad
jlkim909 <jlkim909@gmail.com>
2024-04-05 20:22:10 +0900

    fix : hilt-navigation-compose, lifecycle-runtime-compose 라이브러리 추가

--- app/src/main/java/com/hifi/redeal/memo/repository/PhotoMemosRepository.kt
+++ app/src/main/java/com/hifi/redeal/memo/repository/PhotoMemosRepository.kt
@@ -1,10 +1,11 @@
 package com.hifi.redeal.memo.repository
-
 import com.hifi.redeal.memo.model.PhotoMemo
 import kotlinx.coroutines.flow.Flow
 
 interface PhotoMemosRepository {
     fun getPhotoMemos(): Flow<List<PhotoMemo>>
+
+    fun getClientWithPhotoMemos(clientId:Int): Flow<List<PhotoMemo>>
 
     fun getPhotoMemo(id:Int):Flow<PhotoMemo>
 
