7a304cf2aa6e377ebc98cdb137bf846192cb0ed4
jlkim909 <jlkim909@gmail.com>
2024-04-05 20:10:55 +0900

    fix : PhotoMemo 데이터 모델 생성

---
+++ app/src/main/java/com/hifi/redeal/memo/model/PhotoMemo.kt
@@ -0,0 +1,24 @@
+package com.hifi.redeal.memo.model
+
+import com.hifi.redeal.data.entrie.PhotoMemoEntity
+
+data class PhotoMemo (
+    val id:Int = 0,
+    val clientOwnerId:Int = 0,
+    val memo:String = "",
+    val timestamp: Long = 0,
+    val imageUris:List<String> = emptyList()
+)
+
+fun PhotoMemo.toPhotoMemoEntity() = PhotoMemoEntity(
+    id = id,
+    clientOwnerId = clientOwnerId,
+    memo = memo,
+    timestamp = timestamp,
+    imageUris = imageUris
+)
+enum class BottomButtonState {
+    IDLE,
+    PRESSED,
+    DISABLED
+}
