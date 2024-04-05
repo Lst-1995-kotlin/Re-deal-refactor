a452f9e1afa7c22792439bc8f62c68ae5940374f
jlkim909 <jlkim909@gmail.com>
2024-04-05 20:32:00 +0900

    fix : getClientWithPhotoMemoEntities 추가

--- app/src/main/java/com/hifi/redeal/data/dao/PhotoMemoDao.kt
+++ app/src/main/java/com/hifi/redeal/data/dao/PhotoMemoDao.kt
@@ -5,24 +5,28 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Update
-import com.hifi.redeal.memo.model.PhotoMemo
+import com.hifi.redeal.data.entrie.PhotoMemoEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PhotoMemoDao {
     @Query("SELECT * FROM photoMemos ORDER BY timestamp ASC")
-    fun getPhotoMemoEntities(): Flow<List<PhotoMemo>>
+    fun getPhotoMemoEntities(): Flow<List<PhotoMemoEntity>>
 
+    @Transaction
+    @Query("Select * FROM photoMemos WHERE clientOwnerId = :clientId ORDER BY timestamp ASC")
+    fun getClientWithPhotoMemoEntities(clientId:Int): Flow<List<PhotoMemoEntity>>
     @Query("SELECT * FROM photoMemos WHERE id = :id")
-    fun getPhotoMemoEntity(id: Int): Flow<PhotoMemo>
+    fun getPhotoMemoEntity(id: Int): Flow<PhotoMemoEntity>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertPhotoMemo(photoMemo: PhotoMemo)
+    suspend fun insertPhotoMemo(photoMemo: PhotoMemoEntity)
 
     @Update
-    suspend fun updatePhotoMemo(photoMemo: PhotoMemo)
+    suspend fun updatePhotoMemo(photoMemo: PhotoMemoEntity)
 
     @Delete
-    suspend fun deletePhotoMemo(photoMemo: PhotoMemo)
+    suspend fun deletePhotoMemo(photoMemo: PhotoMemoEntity)
 }
