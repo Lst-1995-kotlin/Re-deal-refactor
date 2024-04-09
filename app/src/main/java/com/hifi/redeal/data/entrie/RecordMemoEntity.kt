e99cee68e4b4c023f1badda3a189ec6d015b727a
jlkim909 <jlkim909@gmail.com>
2024-04-10 00:45:13 +0900

    fix : RecordMemo.asExternalModel() 생성

--- app/src/main/java/com/hifi/redeal/data/entrie/RecordMemoEntity.kt
+++ app/src/main/java/com/hifi/redeal/data/entrie/RecordMemoEntity.kt
@@ -2,6 +2,7 @@ package com.hifi.redeal.data.entrie
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.hifi.redeal.memo.model.RecordMemo
 
 @Entity(tableName = "recordMemos")
 data class RecordMemoEntity(
@@ -13,4 +14,14 @@ data class RecordMemoEntity(
     val duration:Long,
     val audioFilename:String,
     val audioFileUri:String
+)
+
+fun RecordMemoEntity.asExternalModel() = RecordMemo(
+    id = id,
+    clientOwnerId = clientOwnerId,
+    memo = memo,
+    timestamp = timestamp,
+    duration = duration,
+    audioFilename = audioFilename,
+    audioFileUri = audioFileUri
 )
