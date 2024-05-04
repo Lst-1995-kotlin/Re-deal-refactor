f07e031767fdde6aa283eabbe8482a7affa18740
Lst-1995-kotlin <wkfskstjdxo@gmail.com>
2024-05-04 19:20:33 +0900

    refactor: 미사용 import 제거

--- app/src/main/java/com/hifi/redeal/trade/data/repository/TradeClientRepository.kt
+++ app/src/main/java/com/hifi/redeal/trade/data/repository/TradeClientRepository.kt
@@ -4,7 +4,6 @@ import com.hifi.redeal.data.dao.ClientDao
 import com.hifi.redeal.trade.data.model.TradeClientData
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
-import javax.inject.Singleton
 
 class TradeClientRepository @Inject constructor(
     private val clientDao: ClientDao
--- app/src/main/java/com/hifi/redeal/trade/ui/viewmodel/ClientTradeViewModel.kt
+++ app/src/main/java/com/hifi/redeal/trade/ui/viewmodel/ClientTradeViewModel.kt
@@ -2,7 +2,6 @@ package com.hifi.redeal.trade.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import com.hifi.redeal.trade.data.repository.TradeClientRepository
 import com.hifi.redeal.trade.domain.usecase.TradeClientUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -10,7 +9,7 @@ import javax.inject.Inject
 @HiltViewModel
 class ClientTradeViewModel @Inject constructor(
     private val clientUseCase: TradeClientUseCase,
-): ViewModel() {
+) : ViewModel() {
 
     val clients = clientUseCase.getClients().asLiveData()
 
